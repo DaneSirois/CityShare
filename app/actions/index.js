7a430ca8d7ed087db619f38a257b012763d91c94
Hayden Carlson <haydog2@hotmail.com>
2016-11-18 19:03:45 -0800

    Added post update

--- app/actions/index.js
+++ app/actions/index.js
@@ -1,5 +1,6 @@
 export const NEW_MESSAGE = 'NEW_MESSAGE';
 export const SET_USERNAME = 'SET_USERNAME';
+export const POST_UPDATE = 'POST-UPDATE';
 
 
 export function newMessage(message) {
@@ -18,5 +19,13 @@ export function setUsername(broadcast) {
       username: broadcast.username,
       notification: broadcast.notification
     }
+  };
+}
+
+export function postUpdate(update) {
+  console.log(update);
+  return {
+    type: `socket/${POST_UPDATE}`,
+    payload: update
   };
 }
