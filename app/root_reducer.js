fc77bc8bd9264acc407c3b02e9d5cf16c3ea4785
Cliffsky <nickvbn@gmail.com>
2016-11-19 01:53:11 +0000

    clean slate

--- app/root_reducer.js
+++
@@ -1,9 +0,0 @@
-import {combineReducers} from 'redux';
-
-import {Chatroom__reducers} from './modules/Chatroom/Chatroom__API.js';
-
-const root_reducer = combineReducers({ 
-  Chatroom: Chatroom__reducers
-});
-
-export default root_reducer;
