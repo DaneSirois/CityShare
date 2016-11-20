bfcb56b9bc68a83be1a3dd1ced46b82c4b36c424
Cliffsky <nickvbn@gmail.com>
2016-11-20 22:02:26 +0000

    resolving conflicts

--- app/modules/App/App__index.js
+++ app/modules/App/App__index.js
@@ -3,7 +3,7 @@ import React, {Component} from 'react';
 // Styles:
 import App from './App__styles.css';
 
-import Navbar__container from '../Shared/Shared__container__navbar.js';
+import Navbar__container from '../Shared/Shared__container__Navbar.js';
 import Chatroom__module from '../Chatroom/Chatroom__index.js';
 import Portal__module from '../Portal/Portal__index.js';
 
