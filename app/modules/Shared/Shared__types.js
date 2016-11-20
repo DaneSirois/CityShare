4c1c483a70bfa7383614360d123238e9fb35515f
Dane Sirois <dane_sirois@live.com>
2016-11-20 23:34:44 +0000

    Organized the Shared module

--- app/modules/Shared/Shared__types.js
+++ app/modules/Shared/Shared__types.js
@@ -1,25 +1,25 @@
-// App actions:
+// App Types:
 export const GET_INITIAL_STATE = 'GET_INITIAL_STATE';
 
-// Chatroom Actions:
+// Chatroom Types:
 export const NEW_MESSAGE = 'NEW_MESSAGE';
 export const ADD_TO_CHATLOG = 'ADD_TO_CHATLOG';
 
-// Auth Actions:
+// Auth Types:
 export const SET_ACTIVE_FORM = 'SET_ACTIVE_FORM';
 export const FORM_IS_ACTIVE = 'FORM_IS_ACTIVE';
 
-// User Actions:
+// User Types:
 export const SIGNUP_USER = 'SIGNUP_USER';
 export const AUTHENTICATE_USER = 'AUTHENTICATE_USER';
 export const USER_AUTHENTICATED = 'USER_AUTHENTICATED';
 
-// Portal Actions
+// Portal Types:
 export const NEW_CHANNEL = 'NEW_CHANNEL';
 export const GET_CHANNELS = 'GET_CHANNELS';
 export const ADD_TO_CHANNEL_LIST = 'ADD_TO_CHANNEL_LIST';
 
-// Feed Actions
+// Feed Types:
 export const NEW_UPDATE = 'NEW_UPDATE';
 export const ADD_TO_FEEDLIST = 'ADD_TO_FEEDLIST';
 
