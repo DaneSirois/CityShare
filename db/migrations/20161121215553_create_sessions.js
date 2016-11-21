2256feffedc17a64b14ad728d7559c85b87aeef4
Cliffsky <nickvbn@gmail.com>
2016-11-21 22:15:09 +0000

    new migration: sessions

--- db/migrations/20161121215553_create_sessions.js
+++ db/migrations/20161121215553_create_sessions.js
@@ -1,8 +1,12 @@
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.table('users', function(table) {
+    table.specificType('socket_ids', 'jsonb[]');
+  });
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.table('users', function (table) {
+    table.dropColumn('socket_ids');
+  });
 };
