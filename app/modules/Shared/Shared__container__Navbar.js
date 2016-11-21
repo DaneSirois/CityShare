ab347cedc1f7f2688d5a932b7bae95b9dc4e85be
Cliffsky <nickvbn@gmail.com>
2016-11-21 22:48:14 +0000

    fixed merge error

--- app/modules/Shared/Shared__container__Navbar.js
+++ app/modules/Shared/Shared__container__Navbar.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 import {connect} from 'react-redux';
 
 // Styles:
-// import Navbar from './styles/Shared__styles__Navbar.css';
+import Navbar from './styles/Shared__styles__Navbar.css';
 
 import Auth__module from '../Auth/Auth__index.js';
 
