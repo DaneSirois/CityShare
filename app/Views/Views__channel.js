724f1a455882521d885cccff8ca7b81790b43604
Hayden Carlson <haydog2@hotmail.com>
2016-11-21 13:40:42 -0800

    Main routes setup index.js

--- app/Views/Views__channel.js
+++ app/Views/Views__channel.js
@@ -1,8 +1,18 @@
 import React, { Component } from 'react';
-
+import Chatroom__module from '../modules/Chatroom/Chatroom__index.js';
+import Navbar__container from '../modules/Shared/Shared__container__Navbar.js';
+import Feed__module from '../modules/Feed/Feed__index.js';
 class ChannelView extends Component {
 	render() {
-		return <div> Show post {this.props.params.id}</div>
+		return (
+			<div>
+			 	Show post {this.props.params.id}
+			 	<Chatroom__module/>
+			 	<Navbar__container/>
+			 	<Feed__module/>
+			 </div>
+
+		);
 	}
 }
 
--- app/index.js
+++ app/index.js
@@ -5,12 +5,12 @@ import {createStore, applyMiddleware} from 'redux';
 
 import root_reducer from './root_reducer.js';
 import { Router, Route, hashHistory } from 'react-router';
-import routes from './routes';
 import createSocketIoMiddleware from 'redux-socket.io';
 import io from 'socket.io-client';
 import App__module from './modules/App/App__index.js';
 import Loading__module from './modules/Loading/Loading__index.js';
 import Portal__module from './modules/Portal/Portal__index.js';
+import Channel__view from './Views/Views__channel.js';
 import actions from './modules/Shared/actions/index.js';
 
 const socket = io('http://localhost:3000');
@@ -24,6 +24,7 @@ ReactDOM.render(
     <Router history={hashHistory}>
     	<Route path="/" component={Loading__module}/>
       <Route path="/portal" component={Portal__module} />
+      <Route path="/channel/:id" component={Channel__view} />
     </Router>
   </Provider>,
   document.getElementById('SRC')
