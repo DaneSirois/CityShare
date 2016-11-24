fa2bcd8ba597ba3274fef2c0e9cef058dc49ca63
Dane Sirois <dane_sirois@live.com>
2016-11-24 19:05:39 +0000

    Added back button to navbar

---
+++ app/modules/Navbar/Navbar__container__BackButton.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+import style from './styles/index.css';
+
+class BackButton__container extends Component {
+  render() {
+    return (
+      <div className={style.BackButton__container}>
+        <i className="fa fa-arrow-circle-o-left" aria-hidden="true"></i>
+      </div>
+    );
+  };
+};
+
+
+export default BackButton__container;
--- app/modules/Navbar/Navbar_index.js
+++ app/modules/Navbar/Navbar_index.js
@@ -3,6 +3,7 @@ import React, {Component} from 'react';
 
 import styles from './styles/index.css';
 
+import BackButton__container from './Navbar__container__BackButton.js';
 import Auth__module from '../Auth/Auth__index.js';
 import Location__container from './Navbar__container__location.js';
 
@@ -11,6 +12,7 @@ class Navbar__module extends Component {
   render() {
     return (
       <nav className={styles.container}>
+        <BackButton__container />
         <Location__container />
         <Auth__module />
       </nav>
