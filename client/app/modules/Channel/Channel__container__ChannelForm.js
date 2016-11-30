7931f7380ea6ae0ea584acd9b94aa69fee83f601
Cliffsky <nickvbn@gmail.com>
2016-11-30 20:13:06 +0000

    fixed new channel form to include inputsnputs for new topics

--- client/app/modules/Channel/Channel__container__ChannelForm.js
+++ client/app/modules/Channel/Channel__container__ChannelForm.js
@@ -1,6 +1,10 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
 
+import ReactSwipe from 'react-swipe';
+import Dropzone from 'react-dropzone';
+import axios from 'axios';
+
 import * as actions from '../Shared/actions/index.js';
 import style from './styles/index.css';
 
@@ -10,6 +14,8 @@ class ChannelForm__container extends Component {
     this.state = {
       name: '',
       tags: '',
+      headline: '',
+      image_url: ''
     }
   }
 
@@ -23,8 +29,43 @@ class ChannelForm__container extends Component {
 
   handleSubmit(event) {
     event.preventDefault();
-    this.props.handleSubmit(this.state.name, this.state.tags);
+    this.props.handleSubmit(this.state.name, this.state.tags, this.state.headline, this.state.image_url);
     this.setState({ name: '', tags: '' })
+  }
+
+  resetInput () {
+    this.setState({ headline: "" });
+  }
+
+  resetImageURL () {
+    this.setState({ image_url: "localhost:3000/public/images/default-headline-bg.jpg" });
+  }
+
+  handleInput (event) {
+    this.setState({ headline: event.target.value });
+  }
+
+  set_uploadStatus(status) {
+    return this.setState({ upload_status: status });
+  }
+
+  onDrop (imageFile) {
+    this.set_uploadStatus("PENDING");
+    const data = new FormData();
+
+    data.append('action', 'ADD');
+    data.append('param', 0);
+    data.append('secondParam', 0);
+    data.append('file', new Blob(imageFile, { type: 'image/jpeg' }));
+
+    axios.post('http://localhost:3000/upload', data).then((img_url) => {
+      this.setState({ image_url: img_url.data });
+      console.log(this.state.image_url);
+      this.set_uploadStatus("COMPLETE");
+    }).catch((error) => {
+      console.log(error);
+      this.set_uploadStatus("COMPLETE");
+    });
   }
 
   render() {
@@ -34,24 +75,49 @@ class ChannelForm__container extends Component {
           <h2>New Channel</h2>
           <input type="text" onChange={this.handleNameInput.bind(this)} placeholder="Name your channel." />
           <input type="text" onChange={this.handleTagInput.bind(this)} placeholder="List some tags." />
+          <textarea
+              className={style.Headline__new}
+              value={this.state.headline}
+              placeholder={"Enter a new Headline.."}
+              onChange={this.handleInput.bind(this)}
+          />
+            <div className={style.Feed__header__submitBar}>
+              {this.state.image_url !== 'localhost:3000/public/images/default-headline-bg.jpg' ?
+              <div className={style.Uploaded__image__container}>
+                <img className={style.Uploaded__image} src={this.state.image_url} />
+                <div className={style.DeleteButton__container} onClick={this.resetImageURL.bind(this)}>
+                  <i className="fa fa-times" aria-hidden="true"></i>
+                </div>
+              </div> : null}
+
+
+              <Dropzone className={style.Dropzone__container} onDrop={this.onDrop.bind(this)} multiple={false} accept={'image/*'}>
+                <div className={style.AddButton__container} onClick={this.resetImageURL.bind(this)}>
+                  <i className="fa fa-picture-o" aria-hidden="true"></i>
+                </div>
+              </Dropzone>
+            </div>
+
           <button onClick={this.handleSubmit.bind(this)}>Create</button>
         </form>
       </div>
-    );
-  };
-};
+    )
+  }
+}
 
 const mapDispatchToProps = function (dispatch) {
   return {
-    handleSubmit: (name, tags) => {
+    handleSubmit: (name, tags, headline, img_url) => {
       const channelData = {
         name: name,
-        tags: tags.split(' ')
+        tags: tags.split(' '),
+        headline: headline,
+        img_url: img_url
       }
       console.log(channelData);
       dispatch(actions.newChannel(channelData));
     }
   }
-};
+}
 
 export default connect(null, mapDispatchToProps)(ChannelForm__container);
